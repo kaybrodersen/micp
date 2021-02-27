d708e7e438bc90ad4a6529997a5e864c7909e2f2
Kay Brodersen <khbrodersen@gmail.com>
2021-02-27 14:25:50 +0100

    Initial logitnsum.R.

---
+++ R/logitnsum.R
@@ -0,0 +1,29 @@
+# ------------------------------------------------------------------------------
+# An R implementation of the convolution (sum) of two logit-normal densities.
+#
+# Kay H. Brodersen, ETH Zurich, Switzerland
+# $Id: logitnsum.R 19160 2013-03-25 13:18:49Z bkay $
+# ------------------------------------------------------------------------------
+logitnsumcdf <- function(x, mu1, sigma1, mu2, sigma2) {
+  # Cumulative distribution function.
+
+  # Compute the PDF first (since we want the entire pdf rather than just one
+  # value from it, using betaconv is computationally more efficient than using
+  # betasumpdf)
+  res <- 0.001
+  co <- logitnconv(res, mu1, sigma1, mu2, sigma2)
+
+  # Sum the PDF up to point x
+  y <- rep(NA, length(x))
+  for (i in (1:length(x))) {
+    idx <- round(x[i] / res)
+    if (idx < 1) {
+      y[i] <- 0
+    } else if (idx > length(co)) {
+      y[i] <- 1
+    } else {
+      y[i] <- trapz(1:idx, co[1:idx]) * res
+    }
+  }
+  return(y)
+}
