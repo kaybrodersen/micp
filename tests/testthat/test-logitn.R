8f3303e20e32b4e1348b1c31257eaf3181bf50ae
Kay Brodersen <khbrodersen@gmail.com>
2021-04-17 22:47:04 +0200

    Add unit tests for `logitninv()`.

--- tests/testthat/test-logitn.R
+++ tests/testthat/test-logitn.R
@@ -27,20 +27,50 @@ test_that("logitncdf rejects bad input", {
 })
 
 test_that("logitncdf works on healthy input", {
-  # F_logitnormal(x = 0.5 | mu = 2, sigma = 1.5) =
-  # 1/2 * (1 + erf((logit(0.5) - 2) / sqrt(2*1.5^2))) = 0.09121122.
+  # F_logitnormal(x = 0.5 | mu = 2, sigma = 1.5)
+  # = 1/2 * (1 + erf((logit(0.5) - 2) / sqrt(2*1.5^2))) = 0.09121122.
   expect_equal(round(logitncdf(0.5, 2, 1.5), 7), 0.0912112)
   expect_equal(round(logitncdf(c(.5, .5), 2, 1.5), 7), c(0.0912112, 0.0912112))
 })
 
-test_that("logitnpdf is zero left of x = 0 irrespective of the parameters", {
+test_that("logitncdf is the inverse of logitninv", {
+  x <- logitninv(0.5, 2, 1.5)
+  expect_equal(round(logitncdf(x, 2, 1.5), 4), 0.5)
+})
+
+test_that("logitncdf is zero left of x = 0 irrespective of the parameters", {
   expect_equal(logitncdf(c(-0.5, 0), 0, 1), c(0, 0))
   expect_equal(logitncdf(c(-0.5, 0), 1, 1.5), c(0, 0))
   expect_equal(logitncdf(c(-0.5, 0), -1.5, 1.5), c(0, 0))
 })
 
-test_that("logitnpdf is one right of x = 1 irrespective of the parameters", {
+test_that("logitncdf is one right of x = 1 irrespective of the parameters", {
   expect_equal(logitncdf(c(1, 1.5), 0, 1), c(1, 1))
   expect_equal(logitncdf(c(1, 1.5), 1, 1.5), c(1, 1))
   expect_equal(logitncdf(c(1, 1.5), -1.5, 1.5), c(1, 1))
+})
+
+test_that("logitninv rejects bad input", {
+  expect_error(logitninv("foo", 1, 2), "p is not a numeric or integer vector")
+  expect_error(logitninv(1, "foo", 2), "mu is not a numeric or integer vector")
+  expect_error(logitninv(1, 2, "x"), "sigma is not a numeric or integer vector")
+  expect_error(logitninv(0.5, 1, 0), "sigma must be positive")
+  expect_error(logitninv(c(0.4, 0.5), 1, 0), "not yet implemented")
+})
+
+test_that("logitninv is the inverse of logitncdf", {
+  # p = F_logitnormal(x = 0.5 | mu = 2, sigma = 1.5)
+  # => F^-1_logitnormal(p | mu = 2, sigma = 1.5) = 0.5.
+  p <- logitncdf(0.5, 2, 1.5)
+  expect_equal(round(logitninv(p, 2, 1.5), 4), 0.5)
+})
+
+test_that("logitninv is 0 and 1 at its extremes", {
+  expect_equal(logitninv(0, 0, 1), 0)
+  expect_equal(logitninv(1, 0, 1), 1)
+})
+
+test_that("logitninv is NA outside of the [0, 1] interval", {
+  expect_equal(logitninv(-0.1, 0, 1), NA_real_)
+  expect_equal(logitninv(1.1, 0, 1), NA_real_)
 })
