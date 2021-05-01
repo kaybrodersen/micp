27288145aa72517137688ca8cc1b70d8a09b2204
Kay Brodersen <khbrodersen@gmail.com>
2021-05-02 00:09:35 +0200

    Roxygenize logitnsum.R.

--- R/logitnsum.R
+++ R/logitnsum.R
@@ -2,18 +2,30 @@
 #
 # Author: Kay H. Brodersen, ETH Zurich
 
+#' Cumulative distribution function of the convolution (sum) of two logit-normal
+#' variables
+#'
+#' @param x Vector of values.
+#' @param mu1 Location parameter of the first distribution.
+#' @param sigma1 Scale parameter of the first distribution.
+#' @param mu2 Location parameter of the second distribution.
+#' @param sigma2 Scale parameter of the second distribution.
+#'
+#' @return Value of the cumulative distribution function.
+#' @export
+#'
+#' @examples
 logitnsumcdf <- function(x, mu1, sigma1, mu2, sigma2) {
-  # Cumulative distribution function.
+  assert_that(is.vector(x), is.numeric(x))
 
-  # Compute the PDF first (since we want the entire pdf rather than just one
-  # value from it, using betaconv is computationally more efficient than using
-  # betasumpdf)
+  # Compute the PDF once, then reuse it for each value in `x`. This is faster
+  # than recomputing the PDF each time.
   res <- 0.001
   co <- logitnconv(res, mu1, sigma1, mu2, sigma2)
 
-  # Sum the PDF up to point x
-  y <- rep(NA, length(x))
-  for (i in (1:length(x))) {
+  # Sum the PDF up to point `x`.
+  y <- rep(NA_real_, length(x))
+  for (i in seq(length(x))) {
     idx <- round(x[i] / res)
     if (idx < 1) {
       y[i] <- 0
