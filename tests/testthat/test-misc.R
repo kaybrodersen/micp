fa18a43060a0f6d88054f26d59e04d704fa408a3
Kay Brodersen <khbrodersen@gmail.com>
2021-09-18 22:47:14 +0200

    Add further tests for `repmat()`.

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -3,3 +3,13 @@ test_that("repmat returns unaltered input as matrix by default", {
   expect_equal(repmat(matrix(c(1, 2, 3))), matrix(c(1, 2, 3)))
   expect_equal(repmat(c(1, 2, 3)), t(matrix(c(1, 2, 3))))
 })
+
+test_that("repmat replicates row-wise", {
+  expect_equal(repmat(1, 3), matrix(c(1, 1, 1)))
+  expect_equal(repmat(c(1, 2), 3), rbind(c(1, 2), c(1, 2), c(1, 2)))
+})
+
+test_that("repmat replicates column-wise", {
+  expect_equal(repmat(1, 1, 3), t(matrix(c(1, 1, 1))))
+  expect_equal(repmat(c(1, 2), 1, 3), t(matrix(c(1, 2, 1, 2, 1, 2))))
+})
