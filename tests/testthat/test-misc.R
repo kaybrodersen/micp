566007981efa9b0229b4b271652b5d54014d4982
Kay Brodersen <khbrodersen@gmail.com>
2021-09-26 10:54:14 +0200

    Add unit tests for `trapz()` (numerical trapezoid integration).

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -12,6 +12,19 @@ test_that("safesigm works on scalar input", {
   expect_equal(safesigm(c(1, -100)), c(0.7310586, 1e-8), tolerance = 1e-6)
 })
 
+test_that("trapz rejects invalid input", {
+  expect_error(trapz(0, 1), "not greater than or equal to 2")
+  expect_error(trapz(c(0, 1), 1), "length(y) not equal to length(x)",
+               fixed = TRUE)
+})
+
+test_that("trapz returns expected result on healthy input", {
+  # Area of a triangle between (0, 0), (0, 1), (1, 1) is 0.5.
+  expect_equal(trapz(c(0, 1), c(0, 1)), 0.5)
+  # Triangle plus an additional square: 1.5.
+  expect_equal(trapz(c(0, 1, 2), c(0, 1, 1)), 1.5)
+})
+
 test_that("repmat returns unaltered input as matrix by default", {
   expect_equal(repmat(1), matrix(1))
   expect_equal(repmat(matrix(c(1, 2, 3))), matrix(c(1, 2, 3)))
