c72b96b71ced1f9b00e68f47f6110f34694a8624
Kay H. Brodersen <khbrodersen@gmail.com>
2022-03-19 18:51:58 +0100

    Add test for `if (idx > length(co))` condition in `logitnsumcdf()`.

--- tests/testthat/test-logitnsum.R
+++ tests/testthat/test-logitnsum.R
@@ -14,6 +14,7 @@ test_that("logitnsumcdf returns expected result", {
   expect_equal(logitnsumcdf(0, 0, 1, 0, 1), 0)
   expect_equal(round(logitnsumcdf(1, 0, 1, 0, 1), 2), 0.5)
   expect_equal(logitnsumcdf(2, 0, 1, 0, 1), 1)
+  expect_equal(logitnsumcdf(3, 0, 1, 0, 1), 1)
 })
 
 test_that("logitnsumcdf works on vector input", {
